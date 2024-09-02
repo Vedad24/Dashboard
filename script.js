fc7afd7676d1e635abd30748a63fd9a46aed69f3
vedad <vedolino2004@gmail.com>
2024-09-02 18:26:51 +0200

    added more elements to the header

---
+++ script.js
@@ -0,0 +1,17 @@
+let searchButton = document.querySelector(".search-button");
+let clicked = false;
+let search_bar = searchButton.nextElementSibling;
+
+searchButton.addEventListener("click", searchBar = () => {
+    if (clicked == false) {
+        search_bar.style.width = "200px";
+        search_bar.style.transitionDuration = "500ms";
+        clicked = true;
+    }
+    else {
+        search_bar.style.width = "0px";
+        search_bar.style.transitionDuration = "500ms";
+        clicked = false;
+    }
+    
+} )
